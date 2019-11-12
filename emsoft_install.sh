954fb377defd0c841fbe69d4ba7c1acab525af80
willgpaik <44753879+willgpaik@users.noreply.github.com>
2019-11-11 19:14:18 -0500

    Create emsoft_install.sh

---
+++ emsoft_install.sh
@@ -0,0 +1,33 @@
+mkdir -p $PWD/emsoft
+mkdir -p $PWD/emtmp
+mkdir -p $PWD/cmake_build
+
+BASE=$PWD
+BUILDDIR=$BASE/emsoft
+TMPDIR=$BASE/emtmp
+
+# install EMsoft SDK
+wget https://github.com/EMsoft-org/EMsoftSuperbuild/archive/v5.0.0.tar.gz
+tar -xf v5.0.0.tar.gz 
+cd EMsoftSuperbuild-5.0.0/
+mkdir build
+cd build
+$CMAKEDIR/bin/cmake .. -DEMsoft_SDK=$BUILDDIR/EMsoft_SDK -DCMAKE_BUILD_TYPE=Release -DINSTALL_QT5=ON
+make
+sed -i "s|/usr/bin/f95|/opt/rh/devtoolset-4/root/usr/bin/gfortran|g" $BUILDDIR/EMsoft_SDK/superbuild/jsonfortran/Build/Release/CMakeCache.txt
+make -j 2 && make
+
+# install EMsoft
+rm v5.0.0.tar.gz
+wget https://github.com/EMsoft-org/EMsoft/archive/v5.0.0.tar.gz
+tar -xf v5.0.0.tar.gz
+cd EMsoft-5.0.0/
+sed -i "s|git@github.com:EMsoft-org/SHTfile.git|https://github.com/EMsoft-org/SHTfile.git|g" CMakeLists.txt
+mkdir build
+cd build
+$CMAKEDIR/bin/cmake .. -DEMsoft_SDK=$BUILDDIR/EMsoft_SDK -DCMAKE_BUILD_TYPE=Release \
+-DHDF5_DIR=$BUILDDIR/EMsoft_SDK/hdf5-1.8.20-Release/share/cmake \
+-Djsonfortran-gnu_DIR=$BUILDDIR/EMsoft_SDK/jsonfortran-4.2.1-Release/lib64/cmake/jsonfortran-gnu-4.2.1/ \
+-DFFTW3_INSTALL=$BUILDDIR/EMsoft_SDK/fftw-3.3.8 \
+-DCLFortran_DIR=$BUILDDIR/EMsoft_SDK/CLFortran-0.0.1-Release/lib/cmake/CLFortran \
+-DQt5Svg_DIR=$BUILDDIR/lib64
