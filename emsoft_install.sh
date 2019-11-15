572fc72ad350132a8a2bee0be7d61b6341b6553e
willgpaik <44753879+willgpaik@users.noreply.github.com>
2019-11-15 18:02:39 -0500

    Update emsoft_install.sh

--- emsoft_install.sh
+++ emsoft_install.sh
@@ -39,11 +39,11 @@ sed -i "s|git@github.com:EMsoft-org/SHTfile.git|https://github.com/EMsoft-org/SH
 sed -i "s|https://github.com/emsoft-org/EMsoftData|https://github.com/EMsoft-org/EMsoftData.git|g" Source/Test/CMakeLists.txt
 mkdir build
 cd build
-cmake .. -DEMsoft_SDK=$BUILDDIR/EMSoft_SDK -DCMAKE_BUILD_TYPE=Release \
+cmake .. -DEMsoft_SDK=$BUILDDIR/EMsoft_SDK -DCMAKE_BUILD_TYPE=Release \
 -DHDF5_DIR=$BUILDDIR/EMsoft_SDK/hdf5-1.8.20-Release/share/cmake \
 -Djsonfortran-gnu_DIR=$BUILDDIR/EMsoft_SDK/jsonfortran-4.2.1-Release/lib64/cmake/jsonfortran-gnu-4.2.1/ \
 -DFFTW3_INSTALL=$BUILDDIR/EMsoft_SDK/fftw-3.3.8 \
--DCLFortran=$BUILDDIR/EMsoft_SDK/CLFortran-0.0.1-Release/lib/cmake/CLFortran \
+-DCLFortran_DIR=$BUILDDIR/EMsoft_SDK/CLFortran-0.0.1-Release/lib/cmake/CLFortran \
 -Dbcls_DIR=$BUILDDIR/EMsoft_SDK/bcls-0.1-Release/lib/cmake/bcls \
 -DBUILD_SHARED_LIBS=ON \
 -DEMsoft_ENABLE_EMsoftWorkbench=OFF \
